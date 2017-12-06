c5ae342f8419105c78f6975139fb882ad4870fb4
nyo <billy31210@gmail.com>
2017-12-07 02:10:36 +0800

    backup .ssh/config

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BAK_DIRNAME="bak-$(date +%Y%m%dT%H%M%S)"
+STORAGE_PATH="$HOME/Dropbox/backup-private/$BAK_DIRNAME"
+
+echo "backup private to $STORAGE_PATH"
+
+mkdir -p $STORAGE_PATH
+
+mkdir $STORAGE_PATH/.ssh
+cp $HOME/.ssh/config $STORAGE_PATH/.ssh/config
